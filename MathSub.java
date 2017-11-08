1d69e26d76b3e1c5f1f12f5ccdc2172875a8cb18
161015H LEE JIE YING <161015H@sit.nyp.edu.sg>
2017-11-08 11:20:23 +0800

    message

--- MathSub.java
+++ MathSub.java
@@ -1,8 +1,8 @@
 public class MathAdd {
 	
 	public MathAdd {}
-	system.out.println();
+	system.out.println("Hello");
 	static public int add(int a, int b) {   
-        return a + b; 
+        return a - b; 
 	}
 }
